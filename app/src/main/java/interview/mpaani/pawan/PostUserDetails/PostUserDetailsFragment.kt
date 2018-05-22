a02a57dfd8b15d212870a718bd5105ea647d26c0
Pawan Shahani <pawan.shahani@gmail.com>
2018-05-22 17:34:54 +0530

    User ID of the uer who posted the Post extracted successfully using Parcelize.

--- app/src/main/java/interview/mpaani/pawan/PostUserDetails/PostUserDetailsFragment.kt
+++ app/src/main/java/interview/mpaani/pawan/PostUserDetails/PostUserDetailsFragment.kt
@@ -1,17 +1,20 @@
 package interview.mpaani.pawan.PostUserDetails
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import interview.mpaani.pawan.Posts.PostDataDO
 import interview.mpaani.pawan.R
 import interview.mpaani.pawan.abstracts.BaseFragment
+import interview.mpaani.pawan.utils.AppConstants
 
 class PostUserDetailsFragment : BaseFragment(), PostUserDetailsView{
 
 
     //Medium priority NON-UI variables goes below......
-
+    private lateinit var postUserpresenter:PostUserDetailsPresenterImpl
 
 
     //Least priority variables goes below...
@@ -21,12 +24,31 @@ class PostUserDetailsFragment : BaseFragment(), PostUserDetailsView{
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view =  inflater.inflate(R.layout.fragment_user, container, false)
-
-        var postUserpresenter = PostUserDetailsPresenterImpl(this, PostUserDetailsInteractorImpl())
-//        postUserpresenter.setUserID()
-
+        postUserpresenter = PostUserDetailsPresenterImpl(this, PostUserDetailsInteractorImpl())
         return view
     }//onCreateView closes here.....
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //Check if we got the Arguments or not !!!!
+        if(arguments != null) {
+            if (arguments!!.containsKey(AppConstants.SELECTED_POST_EXTRAS)) {
+
+                var selectedPostDO = arguments!![AppConstants.SELECTED_POST_EXTRAS] as PostDataDO
+                postUserpresenter.setUserID(selectedPostDO.userId)
+
+            }//if(arguments.containsKey(AppConstants.POSTS_USER_DETAILS_FRAGMENT_BACKSTACK_TAG)) closes here....
+            else
+                Log.w(TAG, "Arguments does not contain ${AppConstants.SELECTED_POST_EXTRAS}")
+        }//if(arguments != null) closes here....
+        else
+            Log.w(TAG, "Arguments does not contain ${AppConstants.SELECTED_POST_EXTRAS}")
+
+
+    }//onViewCreated closes here.....
 
 
     override fun getUserDetails() {
--- app/src/main/java/interview/mpaani/pawan/PostUserDetails/PostUserDetailsPresenterImpl.kt
+++ app/src/main/java/interview/mpaani/pawan/PostUserDetails/PostUserDetailsPresenterImpl.kt
@@ -1,5 +1,7 @@
 package interview.mpaani.pawan.PostUserDetails
 
+import android.util.Log
+
 class PostUserDetailsPresenterImpl(view:PostUserDetailsView, interactor: PostUserDetailsInteractor) : PostUserDetailsPresenter {
 
     //Medium priority NON-UI variables goes below.....
@@ -7,11 +9,11 @@ class PostUserDetailsPresenterImpl(view:PostUserDetailsView, interactor: PostUse
 
 
     //Least priority variables goes below.....
-    private val TAG:String = "PostUserDetailsPresenterImpl"
+    private val TAG:String = "PostUserDetailsPresenterImpl".toString().substring(0,23)
 
     override fun setUserID(userId: Long) {
         //We will get the USER ID here, lets call the data using interactor & set the data accordingly....
-
+        Log.d(TAG, "Pawan chk USer ID from Parcelize is as Follows: $userId")
 
     }//setUserID closes here....
 
